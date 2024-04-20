95d31782d5ccd392c1d06d11e074e0c8f0fca96d
yuwex <25470828+yuwex@users.noreply.github.com>
2024-04-20 18:51:39 -0400

    make it so players.json gets generated instead of being in a bunch of random commits, start working on player stats

--- backend/app/gamedb.py
+++ backend/app/gamedb.py
@@ -1,19 +1,32 @@
 import json
-import dataclasses
+from dataclasses import dataclass
+from os.path import exists
+
 
 def load_data(path: str) -> dict:
     with open(path, 'r') as f:
         return json.loads(f.read())
 
-
 def write_data(data: dict, path: str = 'backend/players.json') -> None:
     with open(path, 'w') as f:
         f.write(json.dumps(data, indent=2))
 
+@dataclass
+class Stats:
+    xp: int = 0
+    xp_per_second: int = 1
+    
+
 class GameDB:
     def __init__(self, data_path: str = 'backend/players.json'):
         self.data_path = data_path
-        self.data = load_data(data_path)
+        
+        if exists(data_path):
+            self.data = load_data(data_path)
+        else:
+            with open(data_path, 'w') as f:
+                f.write("{}")
+            self.data = {}
     
     def new_user(self, username: str, password: str, account_id: str, _write = False):
         data = {
@@ -41,6 +54,8 @@ class GameDB:
                 return self.data[username]['account_id']
         
         return None
+
+    def get_user_stats(self, id: str) -> Stats: pass
 
 # XP
 # XP per second
